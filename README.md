3de71314ef43c2ffba90dd531122b144fa444798
iwauo <iwauo.tajima@gmail.com>
2020-04-29 00:11:27 +0900

    add content

--- README.md
+++ README.md
@@ -1,2 +1,104 @@
-# laplacian.common-model
+<!-- @head-content@ -->
+# laplacian/common-model
+
 Some value objects and mixins which can be applicable to any domain models.
+
+<!-- @head-content@ -->
+
+<!-- @toc@ -->
+## Table of contents
+1. [Usage](#usage)
+
+1. [Schema model](#schema-model)
+
+1. [Source list](#source-list)
+
+
+
+<!-- @toc@ -->
+
+<!-- @main-content@ -->
+## Usage
+
+Add the following entry to your project definition.
+```yaml
+project:
+  models:
+  - group: laplacian
+    name: common-model
+    version: 1.0.0
+```
+
+
+
+## Schema model
+
+
+### Model overview
+
+The following diagram summarizes the structure of the model:
+![](./doc/image/model-diagram.svg)
+
+
+### Entities
+
+- [**Entity**](<./doc/entities/Entity.md>)
+  エンティティ
+- [**NamedParam**](<./doc/entities/NamedParam.md>)
+  named_param
+- [**NamedValue**](<./doc/entities/NamedValue.md>)
+  named_value
+- [**Property**](<./doc/entities/Property.md>)
+  property
+- [**PropertyMapping**](<./doc/entities/PropertyMapping.md>)
+  property_mapping
+- [**Query**](<./doc/entities/Query.md>)
+  The queries from which all navigation originates.
+- [**Relationship**](<./doc/entities/Relationship.md>)
+  relationship
+- [**ValueDomainType**](<./doc/entities/ValueDomainType.md>)
+  value_domain_type
+- [**ValueDomain**](<./doc/entities/ValueDomain.md>)
+  value_domain
+- [**ValueItem**](<./doc/entities/ValueItem.md>)
+  value_item
+- [**LocalizedText**](<./doc/entities/LocalizedText.md>)
+  localized_text
+- [**MultilingualText**](<./doc/entities/MultilingualText.md>)
+  multilingual_text
+- [**NamedParam**](<./doc/entities/NamedParam.md>)
+  named_param
+- [**NamedValue**](<./doc/entities/NamedValue.md>)
+  named_value
+
+
+
+
+## Source list
+
+
+[model/project/language_code_list.yaml](<./model/project/language_code_list.yaml>)
+
+[model/project/sources.yaml](<./model/project/sources.yaml>)
+
+[model/project.yaml](<./model/project.yaml>)
+
+[src/entities/localized_text.yaml](<./src/entities/localized_text.yaml>)
+
+[src/entities/multilingual_text.yaml](<./src/entities/multilingual_text.yaml>)
+
+[src/entities/named_param.yml](<./src/entities/named_param.yml>)
+
+[src/entities/named_value.yml](<./src/entities/named_value.yml>)
+
+[src/value_domain_types/language.yaml](<./src/value_domain_types/language.yaml>)
+
+[template/src/entities/multilingual_text.yaml.hbs](<./template/src/entities/multilingual_text.yaml.hbs>)
+
+[template/src/value_domain_types/language.yaml.hbs](<./template/src/value_domain_types/language.yaml.hbs>)
+
+
+
+
+
+<!-- @main-content@ -->
---
+++ scripts/generate.sh
@@ -0,0 +1,203 @@
+#!/usr/bin/env bash
+set -e
+PROJECT_BASE_DIR=$(cd $"${BASH_SOURCE%/*}/../" && pwd)
+
+SCRIPT_BASE_DIR="$PROJECT_BASE_DIR/scripts"
+LOCAL_REPO_PATH="$PROJECT_BASE_DIR/../mvn-repo"
+if [[ -d "$PROJECT_BASE_DIR/subprojects/mvn-repo" ]]
+then
+  LOCAL_REPO_PATH="$PROJECT_BASE_DIR/subprojects/mvn-repo"
+fi
+
+NEXT_CONTENT_DIR_NAME='.NEXT'
+NEXT_CONTENT_DIR="$PROJECT_BASE_DIR/$NEXT_CONTENT_DIR_NAME"
+PREV_CONTENT_DIR_NAME='.PREV'
+PREV_CONTENT_DIR="$PROJECT_BASE_DIR/$PREV_CONTENT_DIR_NAME"
+DEST_DIR_NAME='dest'
+SRC_DIR_NAME='src'
+
+HELP=
+VERBOSE=
+DRY_RUN=
+MAXIMUM_RECURSION=5
+RECURSION_COUNT=1
+
+main() {
+  parse_args "$@"
+  ! [ -z $VERBOSE ] && set -x
+  ! [ -z $HELP ] && show_usage && exit 0
+  while ! has_settled
+  do
+    create_next_content_dir
+    update_file_index
+    generate
+    RECURSION_COUNT=$(($RECURSION_COUNT + 1))
+  done
+  [ -z $DRY_RUN ] && apply_next_content
+}
+
+parse_args() {
+  while getopts 'dhvr:-:' OPTION;
+  do
+    case $OPTION in
+    -)
+      case $OPTARG in
+      dry-run)
+        DRY_RUN='yes' ;;
+      max-recursion)
+        MAXIMUM_RECURSION=("${!OPTIND}"); OPTIND=$(($OPTIND+1)) ;;
+      *)
+        echo "ERROR: Unknown OPTION --$OPTARG" >&2
+        exit 1
+      esac
+      ;;
+
+    d) DRY_RUN='yes' ;;
+    h) HELP='yes' ;;
+    v) VERBOSE='yes' ;;
+    r) MAXIMUM_RECURSION=("${!OPTIND}"); OPTIND=$(($OPTIND+1))
+    esac
+  done
+}
+
+show_usage () {
+cat << END
+Usage: $(basename "$0") [OPTION]...
+  -d, --dry-run
+    Generate files into a templatry dirctory preserving the existing content.
+
+  -r, --max-recursion RECURSION_LIMIT
+    The limit of how meny times the generator executed. (Default: 5)
+
+  -h
+    Display this help message.
+
+  -v
+    Verbose output
+END
+}
+
+create_next_content_dir() {
+  if [ $RECURSION_COUNT == 1 ]
+  then
+    rm -rf $NEXT_CONTENT_DIR $PREV_CONTENT_DIR
+    mkdir -p $NEXT_CONTENT_DIR
+    copy_content $PROJECT_BASE_DIR $NEXT_CONTENT_DIR
+  fi
+  copy_content $NEXT_CONTENT_DIR $PREV_CONTENT_DIR
+
+  local src_dir="$NEXT_CONTENT_DIR/$SRC_DIR_NAME"
+  local dest_dir="$NEXT_CONTENT_DIR/$DEST_DIR_NAME"
+
+  rm -rf $dest_dir
+  if [ -d $src_dir ]
+  then
+    cp -rf $src_dir $dest_dir
+  else
+    mkdir -p $dest_dir
+  fi
+}
+
+copy_content() {
+  local src_dir=$1
+  local dest_dir=$2
+  [ -d $src_dir ] || return
+  mkdir -p $dest_dir
+  rm -rf $dest_dir/* || true
+  rm -f $dest_dir/.[^.]* || true
+  [ -z "$(ls -A $src_dir)" ] && return
+  rsync -a${VERBOSE:+v} $src_dir/* $src_dir/.[^.]* $dest_dir \
+             --exclude=.git \
+             --exclude=.PREV \
+             --exclude=.NEXT \
+             --exclude=subprojects/mvn-repo/ \
+             --exclude=tmp/
+}
+
+normalize_path() {
+  local path=$1
+  if [[ $path == ./* ]]
+  then
+    echo "${PROJECT_BASE_DIR}/$path"
+  elif [[ $path == /* ]]
+  then
+    echo $path
+  else
+    echo "$NEXT_CONTENT_DIR/$path"
+  fi
+}
+
+update_file_index() {
+  local index_dir="$NEXT_CONTENT_DIR/model/project"
+  mkdir -p $index_dir
+  cat <<EOF > "$index_dir/sources.yaml"
+project:
+  sources:$(file_list | sort -d)
+EOF
+}
+
+file_list() {
+  (cd "$NEXT_CONTENT_DIR"
+    local separator="\n  - "
+    local dirs=
+    [ -d ./model ] && dirs="$dirs ./model"
+    [ -d ./template ] && dirs="$dirs ./template"
+    [ -d ./src ] && dirs="$dirs ./src"
+    find $dirs -type f | while read -r file
+    do
+      printf "$separator\"${file:2}\""
+    done
+    printf "\n"
+  )
+}
+
+generate() {
+  local generator_script="$PROJECT_BASE_DIR/scripts/laplacian-generate.sh"
+  $generator_script \
+    --plugin 'laplacian:laplacian.project.schema-plugin:1.0.0' \
+    --plugin 'laplacian:laplacian.metamodel-plugin:1.0.0' \
+    --template 'laplacian:laplacian.project.base-template:1.0.0' \
+    --template 'laplacian:laplacian.schema.document-template:1.0.0' \
+    --model 'laplacian:laplacian.project.project-types:1.0.0' \
+    --model 'laplacian:laplacian.project.document-content:1.0.0' \
+    --model 'laplacian:laplacian.metamodel:1.0.0' \
+    --model-files $(normalize_path 'model/project.yaml') \
+    --model-files $(normalize_path 'model/project/') \
+    --model-files $(normalize_path 'src/') \
+    --template-files $(normalize_path 'template/') \
+    --target-dir "$NEXT_CONTENT_DIR_NAME" \
+    --local-repo "$LOCAL_REPO_PATH"
+}
+
+has_settled() {
+  [ $RECURSION_COUNT == 1 ] && return 1
+  [ -d $NEXT_CONTENT_DIR ] || return 1
+  [ -d $PREV_CONTENT_DIR ] || return 1
+  diff -r -x 'build' $NEXT_CONTENT_DIR $PREV_CONTENT_DIR
+}
+
+apply_next_content() {
+  copy_content $PROJECT_BASE_DIR $PREV_CONTENT_DIR
+  copy_content $NEXT_CONTENT_DIR $PROJECT_BASE_DIR
+  rm -rf $NEXT_CONTENT_DIR $PREV_CONTENT_DIR
+}
+
+main "$@"
+
+generate() {
+  local generator_script="$PROJECT_BASE_DIR/scripts/laplacian-generate.sh"
+}
+
+has_settled() {
+  [ -d $NEXT_CONTENT_DIR ] || return 1
+  [ -d $PREV_CONTENT_DIR ] || return 1
+  diff -r -x 'build' $NEXT_CONTENT_DIR $PREV_CONTENT_DIR
+}
+
+apply_next_content() {
+  copy_content $PROJECT_BASE_DIR $PREV_CONTENT_DIR
+  copy_content $NEXT_CONTENT_DIR $PROJECT_BASE_DIR
+  rm -rf $NEXT_CONTENT_DIR $PREV_CONTENT_DIR
+}
+
+main "$@"
